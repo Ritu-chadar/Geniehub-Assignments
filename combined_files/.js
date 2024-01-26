9d2fa557305af5e4f85c99efd36d1eeb1f516e96
Ritu-chadar <rituchadar1206@gmail.com>
2024-01-26 19:52:59 +0530

    Add combined_files directory and move files

---
+++ combined_files/.js
@@ -0,0 +1,37 @@
+let x=6;
+console.log(x);//
+//loops
+for(let i=1; i<=5; i++){
+    console.log("Hello");
+}
+let sum=0;
+for(let i=1;i<=5;i++){
+     sum=sum+i;
+}
+console.log("sum= " ,sum);
+//strings
+
+let str="ritu chadar"
+str.length;
+console .log(str[1]);
+
+//extracting string charcters
+
+let str1="Hello Ritu";
+let char=str1.charAt(1);
+console.log(char);
+
+//extracting string parts
+
+let txt="Ritu,Apoorv,Falguni";
+let part=txt.slice(2,7);
+console.log(txt.slice(5));
+console.log(txt.slice(-11));
+
+
+
+
+
+
+
+
